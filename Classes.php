167a6062d11eba45479d45ae4c4e9d244854445b
Descafeinado <renanblack@hotmail.com>
2015-07-02 16:41:18 -0300

    Facilitando entendimento
    Limpeza de linhas excessivas e melhora na identação para facilitar o
    entendimento do código.

--- Classes.php
+++ Classes.php
@@ -38,8 +38,7 @@ class Artesao {
 		$this->telefone = $resultado->telefone;
 		$this->cep = $resultado->cep;
 		$this->data_nascimento = $resultado->data_nascimento;         
-		
-		
+	
 	}
 	
 	public function getByEmail($email){
@@ -48,72 +47,57 @@ class Artesao {
 		
 	    $resultado = $conexao->consulta("select * from artesaos where email='$email'");
 		
-		
 		if($resultado){
 		
-		$this->id_artesao = $resultado->id_artesao; 
-		$this->cpf = $resultado->cpf;
-		$this->nome = $resultado->nome;
-		$this->email = $resultado->email;
-		$this->rua = $resultado->rua;
-		$this->numero = $resultado->numero;
-		$this->bairro = $resultado->bairro;
-		$this->cidade = $resultado->cidade;
-		$this->estado = $resultado->estado;
-		$this->telefone = $resultado->telefone;
-		$this->cep = $resultado->cep;
-		$this->data_nascimento = $resultado->data_nascimento;
-		return true;
+			$this->id_artesao = $resultado->id_artesao; 
+			$this->cpf = $resultado->cpf;
+			$this->nome = $resultado->nome;
+			$this->email = $resultado->email;
+			$this->rua = $resultado->rua;
+			$this->numero = $resultado->numero;
+			$this->bairro = $resultado->bairro;
+			$this->cidade = $resultado->cidade;
+			$this->estado = $resultado->estado;
+			$this->telefone = $resultado->telefone;
+			$this->cep = $resultado->cep;
+			$this->data_nascimento = $resultado->data_nascimento;
+			return true;
 		
 	   }
 	   else return false;         
 			
 	}
 	
-	
-	
-	
 	public function listByFiltro($nome, $cpf, $cidade){
-		
 		
 		$conexao = new conexao();
 		
-		
-		
 		return $conexao->consulta("select * from artesaos where nome like '%$nome%' and cpf like '%$cpf%' and cidade like '%$cidade%'",'result');
 	   
-		
-		
 	}
 	
-	
-	
 	public function listall(){
-		
 		
 		$conexao = new conexao();
 		
 		return $conexao->consulta("select * from artesaos");
 		
-		
 	}
 	
-
     public function salvar_novo(){
     
          $conexao = new conexao();
          
-         
          $resultado = $conexao->consulta("select * from artesaos where cpf = $this->cpf || email = '$this->email'",'result');
          
-         if($resultado->rowCount() > 0) return false;
+         if($resultado->rowCount() > 0)
+			 return false;
          else{
          
          $conexao->insercao("insert into artesaos(cpf,nome,email,rua,numero,bairro,cidade,estado,telefone,cep,data_nascimento) 
                      values($this->cpf,'$this->nome','$this->email','$this->rua',$this->numero,'$this->bairro','$this->cidade','$this->estado','$this->telefone','$this->cep',date('$this->data_nascimento'))");
                      return true;
          }
-         
      
     } 
     
@@ -181,36 +165,33 @@ class user{
 		
 	    $resultado = $conexao->consulta("select * from users where email='$email'");
 		
-		
 		if($resultado){
 		
-		$this->id_user = $resultado->id_user; 
-		$this->cpf = $resultado->cpf;
-		$this->nome = $resultado->nome;
-		$this->email = $resultado->email;
-		$this->rua = $resultado->rua;
-		$this->numero = $resultado->numero;
-		$this->bairro = $resultado->bairro;
-		$this->cidade = $resultado->cidade;
-		$this->uf = $resultado->uf;
-		$this->perm = $resultado->perm;
-		$this->senha = $resultado->senha;
-		
-		return true;
+			$this->id_user = $resultado->id_user; 
+			$this->cpf = $resultado->cpf;
+			$this->nome = $resultado->nome;
+			$this->email = $resultado->email;
+			$this->rua = $resultado->rua;
+			$this->numero = $resultado->numero;
+			$this->bairro = $resultado->bairro;
+			$this->cidade = $resultado->cidade;
+			$this->uf = $resultado->uf;
+			$this->perm = $resultado->perm;
+			$this->senha = $resultado->senha;
+			
+			return true;
 		
 	   }
-	   else return false;         
+	   else 
+		   return false;         
 			
 	}
 	
-	
 	public function listall(){
-		
 		
 		$conexao = new conexao();
 		
 		return $conexao->consulta("select * from users");
-		
 		
 	}
 	
@@ -227,24 +208,17 @@ class user{
 	
 	public function atualizar(){
 		
-		
 		$conexao = new conexao();
 		
 		$conexao->update("update users set cpf=$this->cpf, nome='$this->nome', email='$this->email', rua='$this->rua', 
 		            numero=$this->numero, bairro='$this->bairro', cidade='$this->cidade', uf='$this->uf', 
 		            perm='$this->perm', senha='$this->senha' where id_user=$this->id_user");
 		
-		
 	}
-	
-	
-	
-	
+
 }
 
-
 class imagem_artesao {
-
 
      public $id_image;
      public $id_artesao;
@@ -253,63 +227,47 @@ class imagem_artesao {
      public $tamanho;
      public $conteudo;
 
-
-
 public function inserir_imagem($nome_campo){
 
 
- if($nome_campo == Null) return false;
+	if($nome_campo == Null)
+		return false;
 //recebe metadados da imagem
-$this->nome_imagem = $_FILES[$nome_campo]["tmp_name"];
-$this->tipo = $_FILES[$nome_campo]["type"];
-$this->tamanho = $_FILES[$nome_campo]["size"];
+	$this->nome_imagem = $_FILES[$nome_campo]["tmp_name"];
+	$this->tipo = $_FILES[$nome_campo]["type"];
+	$this->tamanho = $_FILES[$nome_campo]["size"];
 
 //trata a imagem
 
-$this->conteudo = file_get_contents($this->nome_imagem);
+	$this->conteudo = file_get_contents($this->nome_imagem);
+	$this->conteudo = addslashes($this->conteudo);
+	$this->nome_imagem = $_FILES[$nome_campo]["name"];
 
-
-$this->conteudo = addslashes($this->conteudo);
-
-
-$this->nome_imagem = $_FILES[$nome_campo]["name"];
-
-
-
-   $conexao = new conexao();
+	$conexao = new conexao();
 
    $conexao->insercao("insert into imagens_artesao(id_artesao, nome_imagem, tipo, tamanho, conteudo) values($this->id_artesao,'$this->nome_imagem','$this->tipo','$this->tamanho','$this->conteudo')");
 
-
 }
-
 
 public function trocar_imagem($nome_campo){
 	
-if($nome_campo == Null) return false;
+	if($nome_campo == Null)
+		return false;
 //recebe metadados da imagem
-$this->nome_imagem = $_FILES[$nome_campo]["tmp_name"];
-$this->tipo = $_FILES[$nome_campo]["type"];
-$this->tamanho = $_FILES[$nome_campo]["size"];
+	$this->nome_imagem = $_FILES[$nome_campo]["tmp_name"];
+	$this->tipo = $_FILES[$nome_campo]["type"];
+	$this->tamanho = $_FILES[$nome_campo]["size"];
 
 //trata a imagem
 
-$this->conteudo = file_get_contents($this->nome_imagem);
+	$this->conteudo = file_get_contents($this->nome_imagem);
+	$this->conteudo = addslashes($this->conteudo);
+	$this->nome_imagem = $_FILES[$nome_campo]["name"];
 
+	$conexao = new conexao();
 
-$this->conteudo = addslashes($this->conteudo);
-
-
-$this->nome_imagem = $_FILES[$nome_campo]["name"];
-
-
-
-   $conexao = new conexao();
-
-   $conexao->insercao("update imagens_artesao set nome_imagem='$this->nome_imagem', tipo='$this->tipo', tamanho='$this->tamanho', conteudo ='$this->conteudo' where id_artesao=$this->id_artesao");
-
-
-	
+	$conexao->insercao("update imagens_artesao set nome_imagem='$this->nome_imagem', tipo='$this->tipo', tamanho='$this->tamanho', conteudo ='$this->conteudo' where id_artesao=$this->id_artesao");
+   
 }
 
 
@@ -356,9 +314,7 @@ public function mostrar_imagem(){
   }
   else{
 	  
-	  echo '<img src="img/user.jpg" class="img-circle" width="70px" height="70px" alt=""/>';
-	  
-	  
+	  echo '<img src="img/user.jpg" class="img-circle" width="70px" height="70px" alt=""/>';	  
 	  return false;
 	  
   }
@@ -369,7 +325,6 @@ public function mostrar_imagem(){
 
 class imagem_user {
 
-
      public $id_image;
      public $id_user;
      public $nome_imagem;
@@ -377,15 +332,14 @@ class imagem_user {
      public $tamanho;
      public $conteudo;
 
-
 public function inserir_imagem($nome_campo){
 
 
- if($nome_campo == Null) return false;
+	if($nome_campo == Null) return false;
 //recebe metadados da imagem
-$this->nome_imagem = $_FILES[$nome_campo]["tmp_name"];
-$this->tipo = $_FILES[$nome_campo]["type"];
-$this->tamanho = $_FILES[$nome_campo]["size"];
+	$this->nome_imagem = $_FILES[$nome_campo]["tmp_name"];
+	$this->tipo = $_FILES[$nome_campo]["type"];
+	$this->tamanho = $_FILES[$nome_campo]["size"];
 
 //trata a imagem
 
@@ -397,21 +351,18 @@ fclose($pont);
 
 $this->conteudo = addslashes($this->conteudo);*/ 
 
-$this->conteudo = file_get_contents($this->nome_imagem);
+	$this->conteudo = file_get_contents($this->nome_imagem);
 
 
-$this->conteudo = addslashes($this->conteudo);
+	$this->conteudo = addslashes($this->conteudo);
 
 
-$this->nome_imagem = $_FILES[$nome_campo]["name"];
+	$this->nome_imagem = $_FILES[$nome_campo]["name"];
 
+	$conexao = new conexao();
 
-
-   $conexao = new conexao();
-
-   $conexao->insercao("insert into imagens_user(id_user, nome_imagem, tipo, tamanho, conteudo) values($this->id_user,'$this->nome_imagem','$this->tipo','$this->tamanho','$this->conteudo')");
-
-
+	$conexao->insercao("insert into imagens_user(id_user, nome_imagem, tipo, tamanho, conteudo) values($this->id_user,'$this->nome_imagem','$this->tipo','$this->tamanho','$this->conteudo')");
+   
 }
 
 public function mostrar_imagem(){
@@ -427,13 +378,10 @@ public function mostrar_imagem(){
 	  $resultado = $resultado->fetch(PDO::FETCH_OBJ);
 
       echo '<img align="Right" src="data:image/jpeg;base64,'.base64_encode($resultado->conteudo).'" class="img-circle" width="70px" height="70px" alt=""/>';
-      
+
   }
   else{
-	  
-	  echo '<img align="Right" src="img/user.jpg" class="img-circle" width="70px" height="70px" alt=""/>';
-	  
-	  
+	  echo '<img align="Right" src="img/user.jpg" class="img-circle" width="70px" height="70px" alt=""/>';  
 	  return false;
 	  
   }
@@ -442,33 +390,23 @@ public function mostrar_imagem(){
 
 public function trocar_imagem($nome_campo){
 	
-if($nome_campo == Null) return false;
+	if($nome_campo == Null) 
+		return false;
 //recebe metadados da imagem
-$this->nome_imagem = $_FILES[$nome_campo]["tmp_name"];
-$this->tipo = $_FILES[$nome_campo]["type"];
-$this->tamanho = $_FILES[$nome_campo]["size"];
+	$this->nome_imagem = $_FILES[$nome_campo]["tmp_name"];
+	$this->tipo = $_FILES[$nome_campo]["type"];
+	$this->tamanho = $_FILES[$nome_campo]["size"];
 
 //trata a imagem
+	$this->conteudo = file_get_contents($this->nome_imagem);
+	$this->conteudo = addslashes($this->conteudo);
+	$this->nome_imagem = $_FILES[$nome_campo]["name"];
 
-$this->conteudo = file_get_contents($this->nome_imagem);
-
-
-$this->conteudo = addslashes($this->conteudo);
-
-
-$this->nome_imagem = $_FILES[$nome_campo]["name"];
-
-
-
-   $conexao = new conexao();
+	$conexao = new conexao();
 
    $conexao->insercao("update imagens_user set nome_imagem='$this->nome_imagem', tipo='$this->tipo', tamanho='$this->tamanho', conteudo ='$this->conteudo' where id_user=$this->id_user");
-
-
 	
 }
-
-
 
 public function getImage(){
 	
@@ -490,19 +428,13 @@ public function getImage(){
      return true;
    
    }
-   else return false;
+	else return false;
 	
 }
 
-
-
-
-
 }
-
 
 class produto{
-	
 	
 	public $id_produto;
 	public $id_artesao;
@@ -511,10 +443,7 @@ class produto{
 	public $descricao_produto;
 	public $qtd_estoque;
 	public $valor;
-	
-	
-	
-	
+		
 	public function getById($id){
 		
 		$conexao = new conexao();
@@ -528,8 +457,7 @@ class produto{
 		$this->descricao_produto = $resultado->descricao_produto;
 		$this->qtd_estoque = $resultado->qtd_estoque;
 		$this->valor = $resultado->valor;
-		
-		
+	
 	}
 	
 	public function getByDescricao($descricao){
@@ -544,7 +472,6 @@ class produto{
 		$this->qtd_estoque = $resultado->qtd_estoque;
 		$this->valor = $resultado->valor;
 		
-		
 	}
 	
 	
@@ -556,10 +483,7 @@ class produto{
 		
 		return $resultado;
 		
-		
 	}
-	
-	
 	
 	public function listByFiltro($descricao, $artesao, $estoque){
 		
@@ -569,16 +493,9 @@ class produto{
 		$conexao = new conexao();
 		
 		
-		
 		return $conexao->consulta("select * from produtos_artesao where nome like '%$artesao%' and descricao_produto like '%$descricao%' and qtd_estoque < $estoque",'result');
-	   
-		
-		
+	   	
 	}
-	
-	
-	
-	
 	
 	public function salvar_novo(){
 		
@@ -588,7 +505,6 @@ class produto{
 	    $conexao->insercao("insert into produtos(id_artesao,descricao_produto,qtd_estoque,valor) values($this->id_artesao,'$this->descricao_produto',$this->qtd_estoque,$this->valor)");
 	    
 	    return true;
-		
 		
 	}
 	
@@ -603,14 +519,6 @@ class produto{
 		return true;
 		
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 }
 
 
